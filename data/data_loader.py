Blair-Young/deepspeech.pytorch

Added base code for distribution of model

data/data_loader.py
@@ -8,6 +8,7 @@ import numpy as np
 import scipy.signal
 import torch
 import torchaudio
+import math
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
@@ -221,6 +222,36 @@ class BucketingSampler(Sampler):
 
     def shuffle(self):
         np.random.shuffle(self.bins)
+
+
+class DistributedBucketingSampler(Sampler):
+    def __init__(self, data_source, batch_size=1, num_replicas=None, rank=None):
+        """
+        Samples batches assuming they are in order of size to batch similarly sized samples together.
+        """
+        super(DistributedBucketingSampler, self).__init__(data_source)
+        self.data_source = data_source
+        self.ids = list(range(0, len(data_source)))
+        self.batch_size = batch_size
+        self.bins = [self.ids[i:i + batch_size] for i in range(0, len(self.ids), batch_size)]
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.num_samples = int(math.ceil(len(self.data_source) * 1.0 / self.num_replicas))
+
+    def __iter__(self):
+        for ids in self.bins:
+            np.random.shuffle(ids)
+            yield ids
+
+    def __len__(self):
+        return self.num_samples
+
+    def shuffle(self, epoch):
+        # deterministically shuffle based on epoch
+        g = torch.Generator()
+        g.manual_seed(epoch)
+        self.ids = list(torch.randperm(len(self.ids), generator=g))
+        self.bins = [self.ids[i:i + self.batch_size] for i in range(0, len(self.ids), self.batch_size)]
 
 
 def get_audio_length(path):

igormq/aes-lac-2018

data/data_loader.py
@@ -8,6 +8,7 @@ import numpy as np
 import scipy.signal
 import torch
 import torchaudio
+import math
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
@@ -219,8 +220,38 @@ class BucketingSampler(Sampler):
     def __len__(self):
         return len(self.bins)
 
-    def shuffle(self):
+    def shuffle(self, epoch):
         np.random.shuffle(self.bins)
+
+
+class DistributedBucketingSampler(Sampler):
+    def __init__(self, data_source, batch_size=1, num_replicas=None, rank=None):
+        """
+        Samples batches assuming they are in order of size to batch similarly sized samples together.
+        """
+        super(DistributedBucketingSampler, self).__init__(data_source)
+        self.data_source = data_source
+        self.ids = list(range(0, len(data_source)))
+        self.batch_size = batch_size
+        self.bins = [self.ids[i:i + batch_size] for i in range(0, len(self.ids), batch_size)]
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.num_samples = int(math.ceil(len(self.bins) * 1.0 / self.num_replicas))
+
+    def __iter__(self):
+        offset = self.num_samples * self.rank
+        samples = self.bins[offset:offset + self.num_samples]
+        return iter(samples)
+
+    def __len__(self):
+        return self.num_samples
+
+    def shuffle(self, epoch):
+        # deterministically shuffle based on epoch
+        g = torch.Generator()
+        g.manual_seed(epoch)
+        bin_ids = list(torch.randperm(len(self.bins), generator=g))
+        self.bins = [self.bins[i] for i in bin_ids]
 
 
 def get_audio_length(path):