1ed60f5dd870c96ffa70d1a4490e634be2826212
Igor Macedo Quintanilha <v-igquin@microsoft.com>
2018-05-14 11:44:48 -0300

    Move load_model to model_utils

--- codes/utils/model_utils.py
+++ codes/utils/model_utils.py
@@ -1,4 +1,8 @@
 
+import torch
+from codes.model import DeepSpeech
+from codes import transforms as T
+
 def num_of_parameters(model):
     params = 0
     for p in model.parameters():
@@ -9,3 +13,56 @@ def get_state_dict(model):
     model_is_cuda = next(model.parameters()).is_cuda
     model = model.module if model_is_cuda else model
     return model.state_dict()
+
+def load_model(model_path):
+    config = torch.load(model_path)
+
+    if 'version' in config and config['version'] == '0.0.1':
+        return load_legacy_model(config)
+
+    raise NotImplementedError('Not implemented yet.')
+
+def load_legacy_model(config):
+    assert config['version'] == '0.0.1'
+
+    sample_rate = config['audio_conf']['sample_rate']
+    frame_length = int(sample_rate * config['audio_conf']['window_size'])
+    hop = int(sample_rate * config['audio_conf']['window_stride'])
+    rnn_hidden_size = config['hidden_size']
+    num_rnn_layers = config['hidden_layers']
+    rnn_type = config['rnn_type']
+    labels = config['labels']
+    num_classes = len(labels)
+    bidirectional = config['bidirectional']
+    context = config.get('context', 20)
+    state_dict = config['state_dict']
+
+    model = DeepSpeech(rnn_type=rnn_type,
+                num_classes=num_classes,
+                rnn_hidden_size=rnn_hidden_size,
+                num_rnn_layers=num_rnn_layers,
+                window_size=frame_length,
+                bidirectional=bidirectional,
+                context=context)
+
+
+    # the blacklist parameters are params that were previous erroneously saved by the model
+    # care should be taken in future versions that if batch_norm on the first rnn is required
+    # that it be named something else
+    blacklist = [
+        'rnns.0.batch_norm.module.weight', 'rnns.0.batch_norm.module.bias',
+        'rnns.0.batch_norm.module.running_mean',
+        'rnns.0.batch_norm.module.running_var'
+    ]
+    for b in blacklist:
+        if b in state_dict:
+            del state_dict[b]
+
+    model.load_state_dict(state_dict)
+
+    transforms = T.Compose([T.ToTensor(augment=False, sample_rate=sample_rate),
+                          T.ToSpectrogram(frame_length=frame_length, hop=hop, librosa_compat=True)])
+
+    target_transforms = T.ToLabel(labels)
+
+    return model, transforms, target_transforms
--- test.py
+++ test.py
@@ -10,52 +10,7 @@ from codes.data import AudioDataLoader, AudioDataset
 from codes.decoder import GreedyDecoder
 from codes.model import DeepSpeech
 from codes.transforms import Compose, ToLabel, ToSpectrogram, ToTensor
-
-
-def _load_legacy(config):
-    assert config['version'] == '0.0.1'
-
-    sample_rate = config['audio_conf']['sample_rate']
-    frame_length = int(sample_rate * config['audio_conf']['window_size'])
-    hop = int(sample_rate * config['audio_conf']['window_stride'])
-    rnn_hidden_size = config['hidden_size']
-    num_rnn_layers = config['hidden_layers']
-    rnn_type = config['rnn_type']
-    labels = config['labels']
-    num_classes = len(labels)
-    bidirectional = config['bidirectional']
-    context = config.get('context', 20)
-    state_dict = config['state_dict']
-
-    model = DeepSpeech(rnn_type=rnn_type,
-                num_classes=num_classes,
-                rnn_hidden_size=rnn_hidden_size,
-                num_rnn_layers=num_rnn_layers,
-                window_size=frame_length,
-                bidirectional=bidirectional,
-                context=context)
-
-
-    # the blacklist parameters are params that were previous erroneously saved by the model
-    # care should be taken in future versions that if batch_norm on the first rnn is required
-    # that it be named something else
-    blacklist = [
-        'rnns.0.batch_norm.module.weight', 'rnns.0.batch_norm.module.bias',
-        'rnns.0.batch_norm.module.running_mean',
-        'rnns.0.batch_norm.module.running_var'
-    ]
-    for b in blacklist:
-        if b in state_dict:
-            del state_dict[b]
-
-    model.load_state_dict(state_dict)
-
-    transforms = Compose([ToTensor(augment=False, sample_rate=sample_rate),
-                          ToSpectrogram(frame_length=frame_length, hop=hop, librosa_compat=True)])
-
-    target_transforms = ToLabel(labels)
-
-    return model, transforms, target_transforms
+from codes.utils.model_utils import load_model
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='DeepSpeech transcription')
@@ -89,7 +44,7 @@ if __name__ == '__main__':
 
     torch.set_grad_enabled(False)
 
-    model, transforms, target_transforms = _load_legacy(torch.load(args.model_path))
+    model, transforms, target_transforms = load_model(args.model_path)
     label_encoder = target_transforms.label_encoder
 
     model.eval()
