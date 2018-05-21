2cb38c28938c47c7992f00a5414f5c3523531c57
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2018-05-21 13:35:52 -0300

    Undo vars conversion

--- codes/utils/model_utils.py
+++ codes/utils/model_utils.py
@@ -23,7 +23,7 @@ def load_model(model_path):
     if 'version' in ckpt and ckpt['version'] == '0.0.1':
         return load_legacy_model(ckpt)
 
-    args = edict(vars(ckpt['args']))
+    args = edict(ckpt['args'])
     model = DeepSpeech(**args.config.network.params)
     model.load_state_dict(ckpt['state_dict'])
 
