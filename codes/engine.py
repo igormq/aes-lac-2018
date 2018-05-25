c0a7e9d2a2fc382c293bc62817be501edbba6b94
Igor Macedo Quintanilha <v-igquin@microsoft.com>
2018-05-25 15:59:54 -0300

    added multitask engine

--- codes/engine.py
+++ codes/engine.py
@@ -1,4 +1,4 @@
-
+import copy
 import os
 
 import torch
@@ -7,80 +7,98 @@ from ignite import handlers
 from ignite.engine import Engine
 
 
+def _sanitize_loss(criterion,
+                   out,
+                   targets,
+                   input_percentages,
+                   target_sizes,
+                   average=1):
+    # CTC loss is batch_first = False, i.e., T x B x D
+    out = out.transpose(0, 1)
+    seq_length = out.shape[0]
+    out_sizes = (input_percentages * seq_length).int()
+
+    loss = criterion(out, targets, out_sizes.to('cpu'), target_sizes.to('cpu'))
+
+    loss_sum = (loss / average).sum().item()  # average the loss by minibatch
+
+    inf = float("inf")
+    if loss_sum == inf or loss_sum == -inf:
+        print("WARNING: received an inf loss, setting loss value to 0")
+        loss_sum = 0
+
+    return loss_sum
+
+
 def create_trainer(model, optimizer, criterion, device, **kwargs):
-        data_timer = handlers.Timer(average=True)
-        max_norm = kwargs.get('max_norm', 400)
+    data_timer = handlers.Timer(average=True)
+    max_norm = kwargs.get('max_norm', 400)
 
-        task_weights = kwargs.get('task_weights', [1])
-        is_multi_task = len(task_weights) > 1
+    task_weights = kwargs.get('task_weights', [1])
+    is_multi_task = len(task_weights) > 1
 
-        def _update(engine, batch):
-            if engine.skip_n > 0:
-                engine.skip_n -= 1
-                print(engine.state.iteration)
-                return 'Skipped'
+    if is_multi_task and not isinstance(criterion, list):
+        criterion = [
+            copy.deepcopy(criterion) for _ in range(len(task_weights))
+        ]
 
-            model.train()
+    def _update(engine, batch):
+        if engine.skip_n > 0:
+            engine.skip_n -= 1
+            print(engine.state.iteration)
+            return 'Skipped'
 
-            engine.data_timer.resume()
-            if len(batch) == 4:
-                inputs, targets, input_percentages, target_sizes = batch
-                task = torch.zeros(inputs.shape[0], dtype=torch.int)
-            else:
-                inputs, targets, input_percentages, target_sizes, task = batch
-            inputs.to(device)
-            engine.data_timer.pause()
-            engine.data_timer.step()
+        model.train()
 
-            if is_multi_task:
-                out = model(inputs, task)
-            else:
-                out = model(inputs)
+        engine.data_timer.resume()
+        inputs, targets, input_percentages, target_sizes = batch
+        inputs.to(device)
+        engine.data_timer.pause()
+        engine.data_timer.step()
 
-            # CTC loss is batch_first = False, i.e., T x B x D
-            out = out.transpose(0, 1)
+        out = model(inputs)
 
-            seq_length = out.shape[0]
-            out_sizes = (input_percentages * seq_length).int()
-
-            loss = criterion(out, targets, out_sizes.to('cpu'), target_sizes.to('cpu'))
-
+        if is_multi_task:
             total_loss = 0
             for i in enumerate(task_weights):
-                task_idxs = task == i
+                if len(inputs) > i and len(inputs[i]):
+                    task_loss = _sanitize_loss(
+                        criterion[i],
+                        out[i],
+                        targets[i],
+                        input_percentages[i],
+                        target_sizes[i],
+                        average=inputs[i].shape[0])
+                    total_loss += task_weights[i] * task_loss
+        else:
+            total_loss = _sanitize_loss(
+                criterion,
+                out,
+                targets,
+                input_percentages,
+                target_sizes,
+                average=inputs.shape[0])
 
-                task_loss = loss[task_idxs]
-                task_loss = task_loss / inputs[task_idxs].shape[0]  # average the loss by minibatch
+        # compute gradient
+        optimizer.zero_grad()
+        total_loss.backward()
 
-                task_loss_sum = task_loss.sum()
-                inf = float("inf")
-                if task_loss_sum == inf or task_loss_sum == -inf:
-                    print("WARNING: received an inf loss, setting loss value to 0")
-                    task_loss = 0
-                else:
-                    task_loss = loss.item()
+        # Clipping the norm, avoiding gradient explosion
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
 
-                total_loss += task_weights[i] * task_loss
+        # optimizer step
+        optimizer.step()
 
-            # compute gradient
-            optimizer.zero_grad()
-            total_loss.backward()
+        torch.cuda.synchronize()
 
-            # Clipping the norm, avoiding gradient explosion
-            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+        return total_loss
 
-            # optimizer step
-            optimizer.step()
+    engine = Engine(_update)
+    engine.data_timer = data_timer
+    engine.skip_n = kwargs.get('skip_n', 0)
 
-            torch.cuda.synchronize()
+    return engine
 
-            return loss_value
-
-        engine = Engine(_update)
-        engine.data_timer = data_timer
-        engine.skip_n = kwargs.get('skip_n', 0)
-
-        return engine
 
 def create_evaluator(model, metrics, device=torch.device('cuda')):
     def _inference(engine, batch):
